65be4c18c714e297498f46b95cf45d0f7e4169ce
kdlee02 <60279844+kdlee02@users.noreply.github.com>
2024-12-07 13:50:44 +0900

    Update risks.py

--- risks.py
+++ risks.py
@@ -8,25 +8,18 @@ from sklearn.preprocessing import StandardScaler
 import joblib
 from tensorflow.keras.models import load_model
 
-# Load the data
 risk = pd.read_csv("reduced_asdf.csv")
 
-# Drop the 'Unnamed: 0' column
-#risk = risk.drop(columns=['Unnamed: 0'])
-
-# Convert categorical 'object' columns to 'category' for proper selection and processing
 for col in risk.select_dtypes(include='object').columns:
     risk[col] = risk[col].astype('category')
 
 risk['status'] = risk['status'].astype('int8')
 
-# Redefine numerical and categorical columns based on corrected types
 numerical_columns = risk.select_dtypes(include=['float64', 'int64']).columns
 categorical_columns = risk.select_dtypes(include=['category', 'int8']).columns
 
 target_variable = 'status'
 
-# Age mapping for predictions
 age_labels = {
     '25-34': 0,
     '35-44': 1,
@@ -37,62 +30,41 @@ age_labels = {
     '<25': 6
 }
 
-# Load the binary classification model
-#model = load_model('binary_model.h5')
-#model = load_model('neural.h5')
 model = joblib.load('best_model.pkl')
-
-# Load the scaler, PCA, and label encoder
 scaler = joblib.load('scalers.pkl')
-#pca = joblib.load('pca_4.pkl')
 
 def predict_default(property_value, ltv, income, dtir, interest, age):
-        # Create input array
         input_data = np.array([[property_value, ltv, income, dtir, interest]])
         input_data = np.log1p(input_data)  
-        # Apply scaling and PCA
         input_scaled = scaler.transform(input_data)
-        #input_scaled = pca.transform(input_scaled)
-        # Combine PCA-transformed data with categorical 'age'
         final_input = np.hstack([input_scaled, [[age]]])
-        # Get prediction
         prediction = model.predict(final_input)
         return (prediction >= 0.3).astype(int)
 
-# Streamlit App Title
 st.title("Risk Analysis and Prediction")
 
-# Sidebar for selecting mode
 st.sidebar.title("Navigation")
 mode = st.sidebar.radio("Choose an option", 
                         ["Exploratory Data Analysis (EDA)", "Default Risk Prediction"])
 
-# EDA Section
 if mode == "Exploratory Data Analysis (EDA)":
-    # Sidebar for selecting data type
     st.sidebar.title("EDA Options")
     data_type = st.sidebar.radio("Choose a data type", 
                                  ["Numerical Distribution", "Categorical Distribution", 
                                   "Correlation Heatmap", "Bar Chart (Numerical Variables relationship to Status)", 
                                   "Stacked Bar Chart (Categorical Variables relationship to Status)"])
-
-    # Display histogram for numerical data
     if data_type == "Numerical Distribution":
         st.header("Histograms for Numerical Data")
         selected_num_col = st.selectbox("Select a Numerical Column", numerical_columns)
         fig = px.histogram(risk, x=selected_num_col, nbins=30, title=f'Histogram of {selected_num_col}')
         fig.update_layout(bargap=0.2)
         st.plotly_chart(fig)
-
-    # Display count plot for categorical data
     elif data_type == "Categorical Distribution":
         st.header("Count Plots for Categorical Data")
         selected_cat_col = st.selectbox("Select a Categorical Column", categorical_columns)
         fig = px.histogram(risk, y=selected_cat_col, title=f'Count Plot of {selected_cat_col}')
         fig.update_layout(bargap=0.2)
         st.plotly_chart(fig)
-
-    # Display correlation heatmap for numerical data
     elif data_type == "Correlation Heatmap":
         st.header("Correlation Heatmap")
         imputer = SimpleImputer(strategy='mean')
@@ -113,7 +85,6 @@ if mode == "Exploratory Data Analysis (EDA)":
         )
         st.altair_chart(heatmap, use_container_width=True)
 
-    # Display median bar chart for each status
     elif data_type == "Bar Chart (Numerical Variables relationship to Status)":
         st.header("Bar Chart")
         selected_num_col = st.selectbox("Select a Numerical Column", numerical_columns)
@@ -123,7 +94,6 @@ if mode == "Exploratory Data Analysis (EDA)":
                      labels={selected_num_col: f'Median {selected_num_col}', target_variable: target_variable})
         st.plotly_chart(fig)
 
-    # Display stacked bar chart for categorical data by target variable
     elif data_type == "Stacked Bar Chart (Categorical Variables relationship to Status)":
         st.header("Stacked Bar Chart")
         categorical_column = [col for col in categorical_columns if col != target_variable]
@@ -137,11 +107,9 @@ if mode == "Exploratory Data Analysis (EDA)":
         fig.update_layout(barmode='stack', xaxis_title=selected_cat_col, yaxis_title="Percentage")
         st.plotly_chart(fig)
 
-# Prediction Section
 elif mode == "Default Risk Prediction":
     st.header("Predict Default Status")
 
-    # Input fields for prediction
     property_value = st.number_input("Property Value (부동산가치 억원)", min_value=0.0, value=5.0)
     property_value *= 100000
     ltv = st.number_input("Loan-to-Value Ratio (주택담보인정비율) = (부동산 대출금액 / 주택 담보가치) * 100", min_value=0.0, value=50.0)
@@ -153,7 +121,6 @@ elif mode == "Default Risk Prediction":
     age_category = st.selectbox("Age (나이)", list(age_labels.keys()))
     age_label = age_labels[age_category]
     
-    # Predict button
     if st.button("Predict Default Status"):
         prediction = predict_default(property_value, ltv, income, dtir, interest, age_label)
         if prediction == 1:
